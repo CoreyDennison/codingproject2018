365fe3151d6eccf38a5b792852f07faa400f7875
Corey <cmgdennison@gmail.com>
2018-11-12 15:54:03 +0000

    First commit

--- public/js/helpers/constants.js
+++ public/js/helpers/constants.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:8888/';
-export const CLIENT_ID = '';
+export const BASE_URL = 'https://artistbattleapp.herokuapp.com/';
+export const CLIENT_ID = 'f70e95920ca844198610510d35432b5b';
 export const STATE_KEY = 'spotify_auth_state';
