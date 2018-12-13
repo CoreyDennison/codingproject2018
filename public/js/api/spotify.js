3a7c4f6a46d8ab447bc73211c29ee7f493d4b57e
Corey <cmgdennison@gmail.com>
2018-12-13 20:53:02 +0000

    Added spotify api code

--- public/js/api/spotify.js
+++ public/js/api/spotify.js
@@ -1,5 +1,14 @@
+/*const getUserData = (accessToken, q) => {
+  return fetch(`https:api.spotify.com/v1/me`,  {
+      headers: {
+        'Authorization': `Bearer ${accessToken}`
+      }
+  }).then(response => response.json())
+};*/
+
+
 const getArtist = (accessToken, q) => {
-  return fetch(`https://api.spotify.com/v1/me`, {
+  return fetch(`https://api.spotify.com/v1/search?q=${q}&offset=1`  {
       headers: {
         'Authorization': `Bearer ${accessToken}`
       }
--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -13,10 +13,17 @@ if (!access_token || (state == null || state !== storedState)) {
   window.location = "/";
 } else {
   // wrap this in event listener
-  // document.getEventListener("#artist1").addEventListener("keypress", () => {})
+  document.getEventListener("#artist1").addEventListener("keypress", () => {
+  	if(event.keyCode === 13)
   SpotifyAPI.getArtist(access_token).then(data => {
     //USER_PROFILE.innerHTML = outputTemplate(data);
     // artist1Data = 
     console.log(data);
   });
+}
+
+
+function barFunc() {
+    var text = document.getElementById("artist1").value;
+    document.getElementById("test").innerHTML = text;
 }
