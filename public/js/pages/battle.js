1b3b3af200e88142dd1cb13cd572c91dfd8bbd63
Corey <cmgdennison@gmail.com>
2019-01-09 14:46:23 +0000

    Added toString method to innerHTML line

--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -77,6 +77,6 @@ function makeArray() {
           });            
       }
   }
-  document.getElementById("result").innerHTML = artists;
+  document.getElementById("result").innerHTML = artists.toString();
   });
 }
