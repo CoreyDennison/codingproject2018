6965a6347ace568754fd19c68252d2b07441b449
Corey <cmgdennison@gmail.com>
2019-01-10 15:34:29 +0000

    Added event listener

--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -19,8 +19,7 @@ if (!access_token || (state == null || state !== storedState)) {
   });
 }
 */
-
-function battleFunc() {
+document.getElementById("battleButton").addEventListener("click", function){
     var text = document.getElementById("artist1").value;
     var text2 = document.getElementById("artist2").value;
     document.getElementById("info1").innerHTML = text;
@@ -35,11 +34,38 @@ function enterFunc() {
   }
 }
 
-//Create Javascript array from array in artists.json
-function makeArray() {
-  $.getJSON('../artists.json', function (artists) {
-    var artists = json.artists;  // could be written as const {artists} = json; either
-    savedArtists = artists;
-    document.getElementById("result").innerHTML = JSON.stringify(artists);
-  });
+/*
+function matchData(){
+  fetch("../artists(1).json")
+  .then(function(response){
+    return response.json();
+  })
+   .then(function(data){
+    data.forEach(function(artist){
+      var getInfo1 = document.getElementById("artist1").value;
+    });
+    document.getElementById("info1").innerHTML = ;
+   })
 }
+*/
+//Create Javascript array from array in artists(1).json
+  //.getJSON is a JQuery method
+  $.getJSON('../artists(1).json', function (json) {
+  var artists = [];
+  for (var key in json) {
+      if (json.hasOwnProperty(key)) {
+          var item = json[key];
+          artists.push({
+              /*pushes each object with the key 'name' 
+              and its value in artists(1).json to 
+              JavaScript artists array*/ 
+              name: item.name,
+              popularity: item.popularity,
+              followers: item.followers.total,
+              type: item.type,
+              genres: item.genres,
+              images: item.images
+          });            
+      }
+   }
+  });
