a4fadb34365d8b62c836ff241df0364feb841c63
Corey <cmgdennison@gmail.com>
2019-01-12 16:37:19 +0000

    Added showArray() and result for testing

--- public/js/pages/battle.js
+++ public/js/pages/battle.js
@@ -108,25 +108,25 @@ function showArray(){
 //Create Javascript array from array in artists(1).json
  //.getJSON is a JQuery method
  $.getJSON('../artists(1).json', function (json) {
- var artists = [];
- for (var key in json) {
-     if (json.hasOwnProperty(key)) {
-         var item = json[key];
-         artists.push({
-             /*pushes each object with the key 'name'
-             and its value in artists(1).json to
-             JavaScript artists array*/
-             name: item.name,
-             popularity: item.popularity,
-             followers: item.followers.total,
-             type: item.type,
-             genres: item.genres,
-             images: item.images
-         });
-     }
- }
-   var newArtists = JSON.stringify(artists);
-   document.getElementById("result").innerHTML = newArtists;
- });
-}
+   var artists = [];
+   for (var key in json) {
+       if (json.hasOwnProperty(key)) {
+           var item = json[key];
+           artists.push({
+               /*pushes each object with the key 'name'
+               and its value in artists(1).json to
+               JavaScript artists array*/
+               name: item.name,
+               popularity: item.popularity,
+               followers: item.followers.total,
+               type: item.type,
+               genres: item.genres,
+               images: item.images
+           });
+       }
+   }
+     var newArtists = JSON.stringify(artists);
+     document.getElementById("result").innerHTML = newArtists;
+   });
+  }
 }
